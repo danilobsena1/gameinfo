e1f0663da0ca4f96deccf0fa169390517d9faec2
Danilo Barros <danilobsena2@gmail.com>
2021-10-28 13:15:06 -0300

    versao2.0

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,7 +1,9 @@
+// SETUP  da demo Basic do owl.Carousel.js
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:false,
+    nav:true, //Ativa duas setas para movimentação das imagens.
     responsive:{
         0:{
             items:1
@@ -9,7 +11,7 @@ $('.owl-carousel').owlCarousel({
         600:{
             items:3
         },
-        1000:{
+        1000:{ //Se a tela possuir 1mil pix, exiba 5 elementos.
             items:5
         }
     }
